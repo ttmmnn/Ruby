40b2abf93de1326e8ea9c238948a1f2a46a7d73f
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-03-06 15:09:21 +0000

    [update]Ruby_10章

--- new.rb
+++ new.rb
@@ -1,24 +1,31 @@
 class Car
-
+  
   def move(direction,distance)
     self.turn(direction)
     self.run(distance)
   end
-
-  def turn(direction)
+  
+   def turn(direction)
     puts "#{direction}に曲がります。"
-  end
-
+   end
+   
   def run(distance)
     puts "車で#{distance}キロ走ります。"
   end
 end
 
+
 car = Car.new
 car.move("右",5)
 
 
-#クラスメソッド
+car = Car.new
+car.turn("右")
+
+car = Car.new
+car.run(5)
+
+
 class Car
   def self.run(distance)
     puts "車で#{distance}キロ走ります。"
@@ -30,7 +37,7 @@ Car.run(10)
 
 class Car
   def self.turn(direction)
-    puts "#{direction}に曲がります"
+    puts "#{direction}に曲がります。"
   end
 end
 
