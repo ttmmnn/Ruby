27c150186eb29fe8f5906c77a86c0eb494f37447
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-03-06 06:18:51 +0000

    [update]Rubyを学ぼう_5章

--- hand.rb
+++ hand.rb
@@ -1,12 +1,12 @@
-hand ="グー"
+hand = "グー"
 if hand =="グー"
   puts "出した手はグーです"
 end
 
-if hand !="チョキ"
+if hand != "チョキ"
   puts "出した手はチョキではありません"
 end
 
-if (hand =="グー")||(hand =="パー")
+if (hand=="グー") || (hand=="パー")
   puts "出した手はグーまたはパーです"
 end
--- lesson5.rb
+++ lesson5.rb
@@ -1,9 +1,9 @@
-total_price =100
+total_price = 90
 
-if total_price >100
+if total_price > 100
   puts "みかんを購入。所持金に余りあり。"
-elsif total_price ==100
-  puts "みかんを購入。所持金は0円。"
-else total_price <100
-  puts "みかんを購入することができません"
+elsif total_price == 100
+  puts "みかんを購入。所持金は0円"
+else
+  puts "みかんを購入することは出来ません。"
 end
